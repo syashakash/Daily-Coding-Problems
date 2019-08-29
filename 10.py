b24a035b6df21ca74bd40e7dd49db4ca0e834785
syashakash <akashshivram9@gmail.com>
2019-08-29 20:28:34 +0530

    28 08 2019 sys sleep job scheduler that calls fn after n secs

---
+++ 10.py
@@ -0,0 +1,19 @@
+'''
+This problem was asked by Apple.
+
+Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+'''
+
+from time import sleep
+
+def jobScheduler(func, nSecs):
+    sleep(nSecs)
+    func()
+
+def customFn():
+    print("Sorry for the delay!!!")
+
+if __name__ == "__main__":
+    nSecs=int(input("Enter delay period : "))
+    jobScheduler(customFn, nSecs)
+    
